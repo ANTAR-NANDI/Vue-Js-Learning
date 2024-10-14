22db20f2db56b5e30b228768052375195bcd2031
ANTAR NANDI <antarnandy.2@gmail.com>
2024-10-14 18:30:09 +0600

    data & attribute binding in vue js

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var app = Vue.createApp({
     data()
     {
         return{
-            msg:'Welcome To Vue Js Tutorial'
-        }
+            msg:'Welcome To Vue Js Tutorial',
+            tutorialinfo:{
+                name:"Mamunur Rashid Vue Js Tutorial",
+                link:'https://www.youtube.com/@rimonmath'
+            },
+            htmlcode:'<a href="https://www.youtube.com/@rimonmath">Vue Js Tutorial</a>'
+        };
     }
 });
 app.mount("#app");
